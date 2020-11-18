9606c76025cab52881396db4e3888699b66b7ecf
kuryart <kuryart1@gmail.com>
2020-11-18 20:11:24 -0300

    Subindo mais uma vez para o servidor

--- resources/views/categoria-modal.blade.php
+++ resources/views/categoria-modal.blade.php
@@ -95,7 +95,7 @@
          <div class="form-container">
           <div class="form-body">
             <div class="form-group">
-              <p class="delete-modal-text">Aviso! Esta operação irá excluir a categoria permanentemente. Tem certeza que deseja continuar?</p>
+              <p class="delete-modal-text">Aviso! Esta operação irá excluir a categoria permanentemente. Tem certeza que deseja continuar? </p>
             </div>
           </div>
           <div class="form-footer">
@@ -108,5 +108,3 @@
   
   </div>
 </div>
-
-{{-- teste2 --}}
